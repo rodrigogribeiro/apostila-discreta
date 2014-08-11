9973c0a25c012e33da0966c6146929c698962880
Rodrigo <rodrigogribeiro@gmail.com>
2014-08-11 14:43:03 -0300

    Adicionado suporte a compilação condicional de arquivos

--- code/cap2/Cap2.v
+++ code/cap2/Cap2.v
@@ -1,5 +1,7 @@
 (** Lógica proposicional em Coq **)
 
+Require Import List Arith_base.
+
 (** Sintaxe da lógica proposicional *)
 
 Section Syntax.
@@ -34,3 +36,71 @@ Section Syntax.
   esta representa um termo sintaticamente incorreto e, é rejeitada  por Coq.
   *)
 End Syntax.
+
+Section Semantics.
+  (* Definição dos conectivos *)
+
+  Definition negB (a : bool) : bool := if a then false else true.
+
+  Definition andB (a b : bool) : bool :=
+    match a, b with
+      | true , b' => b'
+      | false , _ => false
+    end.
+
+  Definition orB (a b : bool) : bool :=
+    match a , b with
+      | false, b' => b'
+      | true, _   => true
+    end.
+
+  Definition implB(a b : bool) : bool :=
+    match a , b with
+      | true , false => false
+      | _ , _        => true
+    end.
+
+  Definition iffB (a  b : bool) : bool :=
+    match a , b with
+      | false, false => true
+      | true , true  => true
+      | _ , _ => false
+    end.
+
+  (*  Definição da semântica *)
+
+  Fixpoint vars' (f : Formula) (m : list nat) : list nat :=
+    match f with
+      | Falsum => m
+      | Verum  => m
+      | Var v  => if In_dec eq_nat_dec v m then m else v :: m
+      | Not f' => vars' f' m
+      | And l r => vars' r (vars' l m)
+      | Or l r => vars' r (vars' l m)
+      | Implies l r => vars' r (vars' l m)
+      | Iff l r => vars' r (vars' l m)
+    end.
+
+  Definition vars (f : Formula) : list nat := vars' f nil.
+
+  Definition Assign : Set := list bool.
+
+  Fixpoint eval (a : Assign)(f : Formula) : bool :=
+    match f with
+      | Falsum => false
+      | Verum => true
+      | Var v => nth v a false
+      | Not f' => negB (eval a f')
+      | And l r => andB (eval a l) (eval a r)
+      | Or l r => orB (eval a l) (eval a r)
+      | Implies l r => implB (eval a l) (eval a r)
+      | Iff l r => iffB (eval a l) (eval a r)
+    end.
+
+   (** exemplo **)
+
+  Definition assignEx40 : Assign := true :: true :: false :: nil.
+  Definition formulaEx40 : Formula := Implies (Not (Var 0)) (And (Var 1) (Var 2)).
+
+  Eval compute in eval assignEx40 formulaEx40.
+End Semantics.
